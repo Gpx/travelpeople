1dee8b5a3b4dc9cdbf1cfd6a02ee3c3796322d82
Gpx <polvara@gmail.com>
2018-05-11 09:12:35 +0200

    Deploy

--- src/Grid.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Grid = styled.div`
-  display: grid;
-  grid: 1fr / repeat(4, 1fr);
-  grid-gap: 1em;
-`
-
-export default Grid
--- src/setupTests.js
+++
@@ -1,9 +0,0 @@
-import 'jest-styled-components'
-import 'jest-dom/extend-expect'
-import './fabricators'
-
-jest.mock('axios', () => ({
-  get: jest.fn(() => {
-    throw new Error('Attempt to make an ajax call in tests')
-  }),
-}))
